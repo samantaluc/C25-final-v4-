c5ae72d58d520af8fc05055f82ff80347d135dd6
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-07-23 23:01:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var angle=60;
 
 var ground;
 
-var top_wall;
+var fan,fan1,fan2,fan3;
 var ball;
 
 var btn1;
@@ -28,34 +28,21 @@ function setup() {
   var ground_options ={
     isStatic: true
   };
- 
- 
-/* 
- ground = Bodies.rectangle(0,400,400,20,ground_options);
- World.add(world,ground); */
+  ground = Bodies.rectangle(0,400,400,20,ground_options);
+ World.add(world,ground); 
   btn2 = createImg('up.png');
   btn2.position(20,30);
   btn2.size(50,50);
-  btn2.mouseClicked(vForce);
-  
-   
-  
-  
-
-  //ground =new Ground(200,390,400,20);
- 
- top_wall = new Ground(50,370,80,30);
- top_wall1 = new Ground(150,370,80,30);
-  top_wall2 = new Ground(250,370,80,30);
-  top_wall3 = new Ground(350,370,80,30);
+  btn2.mouseClicked(vForce);  
+ fan = new Ground(50,370,50,30);
+ fan1 = new Ground(150,370,50,30);
+  fan2 = new Ground(250,370,50,30);
+  fan3 = new Ground(350,370,50,30);
 
   ball = Bodies.circle(200,200,20,ball_options);
   World.add(world,ball);
-  
-  
-  
 
-  rectMode(CENTER);
+    rectMode(CENTER);
   ellipseMode(RADIUS);
 }
 
@@ -68,11 +55,11 @@ function draw()
   
 
   ellipse(ball.position.x,ball.position.y,20);
- // rect(ground.position.x,ground.position.y,400,20);
-    top_wall.show();
- top_wall1.show();
-top_wall2.show();
-  top_wall3.show();
+ rect(ground.position.x,ground.position.y,750,20);
+    fan.show();
+fan1.show();
+fan2.show();
+  fan3.show();
   Engine.update(engine);
 }
 
